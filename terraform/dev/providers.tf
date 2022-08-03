8de6e2f38703bb59dd3026b2cb317b4176172156
Charan Reddy <charanreddy@Charans-MacBook-Air.local>
2022-08-03 16:56:04 +0530

    Added TF code

---
+++ terraform/dev/providers.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    version     =   ">= 1.1.0"
+    alias       =   "aws"
+}
+
+provider "azure" {
+    version     =   ">= 1.1.0"
+    alias       =   "az"
+}
+
+provider "gcp" {
+    version     =   ">= 1.1.0"
+    alias       =   "gcp"
+}
